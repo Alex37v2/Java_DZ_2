2a1f24fa5f9ab6523319e6a172d69e2eb7e5ef73
Алексей Александров <Alex37v2@gmail.com>
2022-03-26 00:10:36 +1000

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 120;
+        int amount = 20;
+        int miles = ticket / amount;
+        System.out.println("Бонусных миль:" + miles);
+    }
+}
+
